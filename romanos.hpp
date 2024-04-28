66631b9d183b90a89264f53c964e474265082cc1
pedro menezes <pedromenezes2305@gmail.com>
2024-04-28 16:55:17 -0300

    segundo commit romanos.hpp

--- romanos.hpp
+++ romanos.hpp
@@ -7,4 +7,11 @@ int romanos_para_decimal(char const *num_romano) {
     int decimal = 0;
     int i;
     int length = strlen(num_romano);
+
+    for (i = 0; i < length; i++) {
+        int valor_atual = valor_romano(num_romano[i]);
+        if (valor_atual == -1) {
+            return -1;
+        }
+    }
 }
