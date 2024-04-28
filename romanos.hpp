a207e25bc2b517a74f62678fc63940678b2efb99
pedro menezes <pedromenezes2305@gmail.com>
2024-04-28 17:15:20 -0300

    quinto commit romanos.hpp

--- romanos.hpp
+++ romanos.hpp
@@ -43,3 +43,16 @@ int main() {
     }
     return 0;
 }
+
+int valor_romano(char c) {
+    switch(c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return -1;
+    }
+}
